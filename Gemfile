98e1fd514911c66efa511b40e2f703bb096fbcb0
Dan Vassallo <danielvassallo87@gmail.com>
2014-07-22 12:02:46 -0400

    Refactored / fixed Show vote

--- Gemfile
+++ Gemfile
@@ -14,7 +14,9 @@ gem 'jbuilder', '~> 1.2'
 gem 'bootstrap-sass'
 gem "devise", "~> 3.1.1"
 gem "paperclip", "~> 3.0"
+gem "mini_magick"
 gem 'rmagick', '2.13.2'
+gem 'carrierwave'
 gem 'aws-sdk', '~> 1.20.0'
 gem "masonry-rails", "~> 0.2.0"
 gem "will_paginate", "~> 3.0"
--- app/assets/javascripts/vote.js
+++ app/assets/javascripts/vote.js
@@ -1,9 +1,23 @@
+//INDEX VOTING //
 $(function(){
   $(".vote-btn").on("click", function(){
     var $this= $(this)
       , $box = $this.closest(".box")
       , $votenum = $box.find(".votenum")
       , votesCount = parseInt($box.find(".vote-text").data("vote"));
+    $votenum.html(votesCount + 1);
+
+  });
+});
+
+//SHOW VOTING //
+
+$(function(){
+  $(".vote-btn2").on("click", function(){
+    var $this= $(this)
+      , $panel = $this.closest(".panel")
+      , $votenum = $panel.find(".votenum")
+      , votesCount = parseInt($panel.find(".vote-text").data("vote"));
     $votenum.html(votesCount + 1);
 
   });
